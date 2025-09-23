52756c0f949919cc32420d9cb7b95379baefea14
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-09-23 12:05:18 +0330

    fix: mock key in app

--- src/pages/createUser/page.tsx
+++ src/pages/createUser/page.tsx
@@ -1,33 +1,43 @@
-import { useSearchParams, useNavigate } from "react-router";
-import { DEFAULT_STOCK_KEEPER_ROUTE } from "src/constants";
-import NotPermitted from "./components/NotPermitted";
+// import { useSearchParams, useNavigate } from "react-router";
+// import { DEFAULT_STOCK_KEEPER_ROUTE } from "src/constants";
+// import NotPermitted from "./components/NotPermitted";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import LoadingComponent from "src/components/common/LoadingComponent";
+import { DEFAULT_STOCK_KEEPER_ROUTE } from "src/constants";
 
 const CreateUser = () => {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
-  const k = searchParams.get("k");
+  // const [searchParams] = useSearchParams();
+  // const k = searchParams.get("k");
 
   useEffect(() => {
-    if (k) {
-      try {
-        const metadata = JSON.parse(localStorage.getItem("metadata") || "{}");
-        localStorage.setItem(
-          "metadata",
-          JSON.stringify({ ...metadata, key: k })
-        );
-      } catch {
-        localStorage.setItem("metadata", JSON.stringify({ key: k }));
-      }
+    localStorage.setItem(
+      "metadata",
+      JSON.stringify({ key: "AE23213C7B130F9D756549C05A0B9733" })
+    );
+    navigate(DEFAULT_STOCK_KEEPER_ROUTE, { replace: true });
+  }, []);
 
-      navigate(DEFAULT_STOCK_KEEPER_ROUTE, { replace: true });
-    }
-  }, [k, navigate]);
+  // useEffect(() => {
+  //   if (k) {
+  //     try {
+  //       const metadata = JSON.parse(localStorage.getItem("metadata") || "{}");
+  //       localStorage.setItem(
+  //         "metadata",
+  //         JSON.stringify({ ...metadata, key: k })
+  //       );
+  //     } catch {
+  //       localStorage.setItem("metadata", JSON.stringify({ key: k }));
+  //     }
 
-  if (!k) {
-    return <NotPermitted />;
-  }
+  //     navigate(DEFAULT_STOCK_KEEPER_ROUTE, { replace: true });
+  //   }
+  // }, [k, navigate]);
+
+  // if (!k) {
+  //   return <NotPermitted />;
+  // }
 
   return <LoadingComponent />;
 };
